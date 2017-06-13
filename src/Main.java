5641f8f392a0f10bde8394c2037aa3717a5b2589
wangyingying30 <wangyingying8@jd.com>
2017-06-13 19:24:39 +0800

    减少控制

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World! wo yao ti jiao dai ma ");
 
-        System.out.println("Hello World! twice");
+
+
+
     }
 
 
