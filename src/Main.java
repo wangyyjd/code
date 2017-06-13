8cc6014762fdad9d8209d3fbd5bc7808ec965747
wangyingying30 <wangyingying8@jd.com>
2017-06-13 19:35:51 +0800

    减少控制

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! 我已经不知道是第几次在线下改动了");
         System.out.println("Hello World! wo yao ti jiao dai ma ");
 
 
