f45a0d9d163a366bb0f295d71cba741f0cf6997d
wangyingying30 <wangyingying8@jd.com>
2017-06-13 19:23:20 +0800

    first change

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World! wo yao ti jiao dai ma ");
+
+        System.out.println("Hello World! twice");
     }
+
+
 }
