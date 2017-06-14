38cb6331161c905b98647bc32d4e57e5b092cef0
wangyingying30 <wangyingying8@jd.com>
2017-06-14 10:47:14 +0800

    add new file

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
         System.out.println("Hello World! ayaya 我是为了制造冲突的 我是在线上修改的");
         System.out.println("Hello World! 我已经不知道是第几次在线下改动了");
         System.out.println("Hello World! wo yao ti jiao dai ma ");
-            
+
 
     }
 
 
+    public void testAdd() {
+        System.out.println("test add method");
+    }
 }
